c96bf985763bc1cae1b1ce83b94c8ff7867e52aa
Jason Kölker <jason@koelker.net>
2010-12-09 16:42:25 -0600

    expost get_holland_config

--- holland/core/config/__init__.py
+++ holland/core/config/__init__.py
@@ -1,9 +1,10 @@
 from config import hollandcfg, setup_config, load_backupset_config, \
-                   BaseConfig, ConfigError
+                   get_holland_config, BaseConfig, ConfigError
 from configobj import ConfigObj, ParseError, ConfigObjError
 
 __all__ = [
     'hollandcfg',
+    'get_holland_config',
     'setup_config',
     'load_backupset_config',
     'BaseConfig'
